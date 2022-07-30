4ea827945090566b1d60b66daa06df9ee6f4df21
KlukaCross <Jktu332@yandex.ru>
2022-07-30 20:54:06 +0300

    VkDynamicCover 0.0.2

--- VkDynamicCover/__init__.py
+++ VkDynamicCover/__init__.py
@@ -1,6 +1,6 @@
 from .dynamic_cover import DynamicCover
 
 __author__ = "Oleg Mezenin"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __copyright__ = "Copyright (c) 2022 Oleg Mezenin"
 __license__ = "MIT"
