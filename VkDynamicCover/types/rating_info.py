1784cc0e235276a3ef594396992e0f51524d7221
KlukaCross <Jktu332@yandex.ru>
2022-11-25 11:55:31 +0300

    add rating_info

---
+++ VkDynamicCover/types/rating_info.py
@@ -0,0 +1,9 @@
+import typing
+
+
+class RatingInfo:
+    def __init__(self, period: str, ban_list: list, point_formula: str):
+        self.period = period
+        self.ban_list = ban_list
+        self.point_formula = point_formula
+        self.points: typing.Dict[int, int] = {}
